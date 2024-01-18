56cf35039cc8f8a5c0964fae4f0e39b9c0c66f46
SSDIntisar2073 <Intisar.Hadhrami@globcom-oman.com>
2024-01-18 10:15:11 +0400

    sss

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,30 +8,32 @@ import { Router } from '@angular/router';
 
 import Swal from 'sweetalert2';
 import { LoginService } from '../login.service';
-
+import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private loginService: LoginService,
-    private router: Router
-  ) {
+  constructor( private formBuilder: FormBuilder,private loginService: LoginService,private router: Router) {
     // Initialize the loginForm in the constructor
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
+
+
   }
 
   ngOnInit(): void {
     // You can keep the ngOnInit lifecycle hook empty if not needed
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
   onSubmit(): void {
